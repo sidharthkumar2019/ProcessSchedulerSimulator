f61653af17bfcd978015f55a8ab45936930f5ddd
sidharthkumar2k19 <sidharthkumar2k19@gmail.com>
2022-01-25 21:41:07 +0530

    Finally, completed

---
+++ README.md
@@ -0,0 +1,13 @@
+This is the process scheduling simulator for OOPs + OS assignment made by Sidharth Kumar (19/11/EC/044). 
+
+It simulates 3 scheduling algorithms :-
+    1. First Come First Serve
+    2. Shortest Remaing Time First
+    3. Round Robin
+
+To run the program, just download this repo's code and compile "main.cpp" in the project folder. You will be prompted for input required for the simulation. Then in "result.txt", you will find the final results of the simulation (CT, TAT, WT, RT, etc) and in "status.txt" the various events will be logged.
+
+3 main classes used :- 
+    1. Process
+    2. ProcessCreator
+    3. Simulator
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<algorithm>
 #include <fstream>
 using namespace std;
 
@@ -7,13 +8,14 @@ class Process {
     public: 
 
     int pid, arrival_time, burst_time, completion_time, tot_turn_around_time,
-        waiting_time, response_time;
+        waiting_time, response_time, copy_burst_time;
     static int process_count;
 
     Process (int mx_at, int mx_bt) {    
         pid = ++process_count;
         arrival_time = rand()%(mx_at-0 + 1) + 0;
         burst_time = rand()%(mx_bt-1 + 1) + 1;
+        copy_burst_time = burst_time; // so that the original BT is not lost
         completion_time = tot_turn_around_time = waiting_time = response_time = -1;
     }   
 
@@ -30,6 +32,13 @@ class Process {
     }
 };
 
+int Process::process_count = 0;
+
+bool compare_process(Process *a, Process *b) {
+    // for sorting the process list wrt arrival time
+    return a->arrival_time < b->arrival_time;
+}
+
 class ProcessCreator {
     public:
     Process **process_list;
@@ -43,9 +52,12 @@ class ProcessCreator {
     }
 
     Process **create () {
-        process_list = new Process* [num_process];
+        process_list = new Process* [num_process];  // array of pointers to 'Process' objects
         for (int i=0 ; i<num_process ; i++)
             process_list[i] = new Process(mx_at, mx_bt);
+        
+        // sort the list wrt AT
+        sort(process_list, process_list+num_process, compare_process);
 
         return process_list;
     }
@@ -60,19 +72,16 @@ class ProcessCreator {
     }
 };
 
-class compare_FCFS {
-    public:
-    bool operator() (Process *A, Process *B) {
-        return A->arrival_time > B->arrival_time;
-    }
-};
-
-class compare_SJF {
+class compare_SRTF {
     public:
     bool operator() (Process *A, Process *B) {
         return A->burst_time > B->burst_time;
     }
 };
+
+bool compare_PID (Process *a, Process *b) {
+    return a->pid < b->pid;
+}
 
 class Simulator{
     int simulation_time, time_quantum; // in ms
@@ -80,7 +89,7 @@ class Simulator{
     char scheduling_algo;
     /*
         'f' -> FCFS
-        's' -> SJF
+        's' -> SRTF
         'r' -> RR
     */
     public:
@@ -110,15 +119,15 @@ class Simulator{
         if ( scheduling_algo == 'f' ) {
             // First come first serve
 
-            priority_queue<Process*, vector<Process*>, compare_FCFS> ready_queue;
-            for (int i=0 ; i<num_process ; i++)
-                ready_queue.push( process_list[i] );
+            // priority_queue<Process*, vector<Process*>, compare_FCFS> ready_queue;
+            // for (int i=0 ; i<num_process ; i++)
+            //     ready_queue.push( process_list[i] );
 
             ofstream f;
             f.open("status.txt");
-            int i = 0;
-            while (!ready_queue.empty())    {
-                Process *t = ready_queue.top();
+            int i = 0, process_ptr = 0;
+            while ( process_ptr<num_process )    {
+                Process *t = process_list[process_ptr];
 
                 if ( t->arrival_time >= i ) {
                     i = t->arrival_time;
@@ -134,7 +143,7 @@ class Simulator{
                 t->tot_turn_around_time = t->completion_time - t->arrival_time;
                 t->waiting_time = t->tot_turn_around_time - t->burst_time;
 
-                ready_queue.pop();
+                process_ptr ++;
             }
             f.close();
 
@@ -142,34 +151,55 @@ class Simulator{
         }
 
         else if ( scheduling_algo == 's' ) {
-            // Shortest Job First
+            // Shortest Remaining Time First
 
-            priority_queue<Process*, vector<Process*>, compare_SJF> ready_queue;
-            for (int i=0 ; i<num_process ; i++)
-                ready_queue.push( process_list[i] );
+            priority_queue<Process*, vector<Process*>, compare_SRTF> ready_queue;
+            // processes will be pushed in the ready queue as they arrive
 
             ofstream f;
             f.open("status.txt");
-            int i = 0;
-            while (!ready_queue.empty())    {
-                Process *t = ready_queue.top();
-                // t->print_info();
+            int process_list_ptr = 0;
 
-                if ( t->arrival_time >= i ) {
-                    i = t->arrival_time;
-                    t->response_time = 0;
+            for ( int i=0 ; i <= simulation_time ; ) {
+                while ( (process_list_ptr < num_process) and 
+                            process_list[process_list_ptr]->arrival_time == i) {
+                    f << "Process with pid: " << process_list[process_list_ptr]->pid << ", " << " arrived at " << process_list[process_list_ptr]->arrival_time << "\n";    
+                    ready_queue.push(process_list[process_list_ptr]);
+
+                    process_list_ptr ++;
                 }
-                else t->response_time = i - t->arrival_time;
 
-                f << "Process with pid: " << t->pid << ", " << " arrived at " << t->arrival_time << "\n";
-                f << "Process with pid: " << t->pid << ", " << " executing at " << i << "\n";
-                i += t->burst_time;
-                f << "Process with pid: " << t->pid << ", " << " exited at " << i << "\n";
-                t->completion_time = i;
-                t->tot_turn_around_time = t->completion_time - t->arrival_time;
-                t->waiting_time = t->tot_turn_around_time - t->burst_time;
+                if ( !ready_queue.empty() ) {
+                    // some processes are there in the ready queue
+                    Process *t = ready_queue.top();
+                    ready_queue.pop();
+                    if ( t->burst_time > 0 )
+                        f << "Process with pid: " << t->pid << ", " << " executing at " << i << "\n";
 
-                ready_queue.pop();
+                    if (t->response_time == -1)
+                        t->response_time = i - t->arrival_time;
+
+                    if (t->burst_time == 0) {
+                        f << "Process with pid: " << t->pid << ", " << " completed execution at " << i << "\n";
+                        t->completion_time = i;
+                        t->tot_turn_around_time = t->completion_time - t->arrival_time;
+                        t->waiting_time = t->tot_turn_around_time - t->copy_burst_time;
+                        t->burst_time = t->copy_burst_time; // for priting in the result
+                    }
+                    else {
+                        // BT is decremented in else because take for example, a process
+                        // with BT = 1 and arrives at 0, then it will execute at time=0 
+                        // and finish executing at t=1. If we decremented BT before the 
+                        // 'if ( t->burst_time == 0 )' condition then CT would have been
+                        // 0 and hence, TAT = 0 and WT = -1, which would be wrong.
+                        t->burst_time --;
+                        ready_queue.push(t);
+                    }                        
+                }   
+                else 
+                    f << "Ready queue is empty (at " << i << ") ...\n";
+
+                i++;
             }
             f.close();
 
@@ -179,46 +209,72 @@ class Simulator{
         else if ( scheduling_algo == 'r' ) {
             // Round Robin
 
-            priority_queue<Process*, vector<Process*>, compare_FCFS> temp;
-            for (int i=0 ; i<num_process ; i++)
-                temp.push( process_list[i] );
-
-            queue<Process *> ready_queue;
-            while (!temp.empty())   {
-                ready_queue.push( temp.top() );
-                temp.pop();
-            }
+            // processes will be pushed in the ready queue as they arrive
+            queue<Process *> ready_queue;   
             
             ofstream f;
             f.open("status.txt");
-            int i = 0;
-            while (!ready_queue.empty())    {
-                Process *t = ready_queue.front();
-                ready_queue.pop();
-                // t->print_info();
+            int process_list_ptr = 0;
 
-                if ( t->arrival_time >= i ) {
-                    i = t->arrival_time;
-                    t->response_time = 0;
+            for ( int i=0 ; i <= simulation_time ; ) {
+                while ( (process_list_ptr < num_process) and 
+                            process_list[process_list_ptr]->arrival_time <= i) {
+                    f << "Process with pid: " << process_list[process_list_ptr]->pid << ", " << " arrived at " << process_list[process_list_ptr]->arrival_time << "\n";    
+                    ready_queue.push(process_list[process_list_ptr]);
+
+                    process_list_ptr ++;
                 }
-                else t->response_time = i - t->arrival_time;
 
-                f << "Process with pid: " << t->pid << ", " << " arrived at " << t->arrival_time << "\n";
-                f << "Process with pid: " << t->pid << ", " << " executing at " << i << "\n";
+                if ( !ready_queue.empty() ) {
+                    // some processes are there in the ready queue
+                    Process *t = ready_queue.front();
+                    ready_queue.pop();
+                    if ( t->burst_time > 0 )
+                        f << "Process with pid: " << t->pid << ", " << " executing at " << i << "\n";
 
-                if ( t->burst_time > time_quantum ) {
-                    i += time_quantum;
-                    t->burst_time -= time_quantum;
-                    ready_queue.push(t);
-                }
+                    if (t->response_time == -1)
+                        t->response_time = i - t->arrival_time;
+
+                    if (t->burst_time == 0) {
+                        f << "Process with pid: " << t->pid << ", " << " completed execution at " << i << "\n";
+                        t->completion_time = i;
+                        t->tot_turn_around_time = t->completion_time - t->arrival_time;
+                        t->waiting_time = t->tot_turn_around_time - t->copy_burst_time;
+                        t->burst_time = t->copy_burst_time; // for priting in the result
+                    }
+                    else {
+                        // BT is decremented in else because take for example, a process
+                        // with BT = 1 and arrives at 0, then it will execute at time=0 
+                        // and finish executing at t=1. If we decremented BT before the 
+                        // 'if ( t->burst_time == 0 )' condition then CT would have been
+                        // 0 and hence, TAT = 0 and WT = -1, which would be wrong.
+                        if ( t->burst_time >= time_quantum ) {
+                            i += (time_quantum-1);
+                            t->burst_time -= time_quantum;
+                        }
+                        else {
+                            i += (t->burst_time -1);
+                            t->burst_time = 0;
+                        }
+
+                        while ( (process_list_ptr < num_process) and 
+                                    process_list[process_list_ptr]->arrival_time <= i) {
+                            f << "Process with pid: " << process_list[process_list_ptr]->pid << ", " << " arrived at " << process_list[process_list_ptr]->arrival_time << "\n";    
+                            ready_queue.push(process_list[process_list_ptr]);
+
+                            process_list_ptr ++;
+                        }
+
+                        ready_queue.push(t);
+                    }                        
+                }   
                 else {
-                    i += t->burst_time;
-                    f << "Process with pid: " << t->pid << ", " << " exited at " << i << "\n";
-                    t->completion_time = i;
-                    t->tot_turn_around_time = t->completion_time - t->arrival_time;
-                    t->waiting_time = t->tot_turn_around_time - t->burst_time;
-                }                
-            }
+                    if (process_list_ptr == num_process and ready_queue.empty())
+                        break;
+                    else f << "Ready Queue is empty (at " << i << ") ...\n";
+                } 
+                i++;
+            }            
             f.close();
 
             generate_report(process_list, num_process);
@@ -235,31 +291,37 @@ class Simulator{
         f.open ("result.txt");
         f << "pid\t|\tAT\t|\tBT\t|\tCT\t|\tTAT\t|\tWT\t|\tRT\n";
         f << "___________________________________________________\n";
-        for (int i=0 ; i<size ; i++) 
+
+        sort( process_list, process_list+size, compare_PID );
+
+        for (int i=0 ; i<size ; i++) {
+            if ( scheduling_algo == 's' )
+                process_list[i]->burst_time = process_list[i]->copy_burst_time;
+
             f << process_list[i]->pid << "\t|\t" << process_list[i]->arrival_time << "\t|\t" 
             << process_list[i]->burst_time << "\t|\t" << process_list[i]->completion_time 
             << "\t|\t" << process_list[i]->tot_turn_around_time << "\t|\t" 
             << process_list[i]->waiting_time << "\t|\t" << process_list[i]->response_time << "\n";
+        }
+            
         f.close();
     }
 };
 
-int Process::process_count = 0;
-
 int main () {
     cout << "Enter simulation time(in seconds): ";
-    int sim_time; cin >> sim_time;
+    int time; cin >> time;
     cout << "Enter the type of scheduling algo that you would like to simulate: ";
-    cout << "Options: \n\t'f' -> FCFS\n\t's' -> SJF\n\t'r' -> RR\n<-";
+    cout << "Options: \n\t'f' -> FCFS\n\t's' -> SRTF\n\t'r' -> RR\n<- ";
     char input; cin >> input;
     if ( input == 'f' or input == 's'){
-        Simulator s(sim_time, input);
+        Simulator s(time, input);
         s.start();
     }
     else if (input == 'r') {
         cout << "Enter the time quantum(in ms): ";
         int tq; cin >> tq;
-        Simulator s(sim_time, input, tq);
+        Simulator s(time, input, tq);
         s.start();
     }    
     return 0;
